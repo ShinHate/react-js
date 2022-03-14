9ee37cf1ce352828caea44dc3ef219c2b7ca92b2
Shin <ShinHate@gmail.com>
2022-03-14 13:04:51 +0300

    commit bases lvl react - fix bagg app.js - filter

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-const AppHeader = () => {
+import './app-header.css';
+
+const AppHeader = ({liked, allPosts}) => {
     return (
         <div className="app-header d-flex">
             <h1>Shin Hate</h1>
-            <h2>5 записей, из них понравилось 0</h2>
+            <h2>{allPosts} записей, из них понравилось {liked}</h2>
         </div>
     )
 }
